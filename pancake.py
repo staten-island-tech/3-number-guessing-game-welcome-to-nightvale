d0ef874d3087dbc3923958af0c6fd003ba37f599
screaming in agony <ameliar51@nycstudents.net>
2026-02-09 10:17:33 -0500

    screa,

---
+++ pancake.py
@@ -0,0 +1,14 @@
+import random
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+num = random.choice(list)
+
+user = int(input('guess the number  '))
+def check(x):
+    x = user
+    if (x) = (num):
+        print("yayyayayayyyay you got it right !!!!")
+    else:
+        print("WRONGe")
+
+check(user)
