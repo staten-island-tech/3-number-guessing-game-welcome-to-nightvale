acd438b5914daceb4657d8ddaa7d044783a8ab6d
screaming in agony <ameliar51@nycstudents.net>
2026-02-09 21:51:51 -0500

    oh joys!

---
+++ bacon.py
@@ -0,0 +1,27 @@
+""" count = 1
+while count <= 5:
+    print("This is loop number", count)
+    count = count + 1 """
+
+""" number = 1
+while number > 0:
+    print("nuu its not stopping") """
+
+""" order = ""
+
+while order != "done":
+    order = input("What would you like to order? (type 'done' to finish): ")
+
+print("Thanks for your order!") """
+
+""" count = 10
+while count >= 1:
+    print(count)
+    count = count - 1 """
+
+""" color = input("Whats your favorite color?  ")
+
+while color != "stop":
+    color = input("This AI-generated challenge has requested a nonsensical challenge that requires me to ask you for another color. Please enter another color  ")
+
+print("Thank you for ending this pointless code!") """
--- pancake.py
+++ pancake.py
@@ -1,14 +1,34 @@
 import random
 
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+"""list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 num = random.choice(list)
 
 user = int(input('guess the number  '))
 def check(x):
     x = user
-    if (x) = (num):
-        print("yayyayayayyyay you got it right !!!!")
-    else:
+    while x != num:
         print("WRONGe")
+    
+    print("yayyayayayyyay you got it right !!!!")
 
-check(user)
+check(user) """
+#above does not work(properly)
+
+""" list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+num = random.choice(list)
+user = int(input("guess the number  "))
+guess_history = []
+def guess(x):
+    while x != num:
+        guess_history.append(x)
+        print(guess_history)
+        if x < num:
+            x = int(input("WRONge!! guess again, the number is greater than your guess  "))
+        else:
+            x = int(input("WRONge!! guess again, the number is greater than your guess  "))
+    guess_history.append(num)
+    print("indeed so! you are correct")
+    print("Guess history:")
+    print(guess_history)
+
+guess(user) """
